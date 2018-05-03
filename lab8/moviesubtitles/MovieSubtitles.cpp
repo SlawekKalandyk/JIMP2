0adcd55f0daae75cee290de0bf4941418c414c83
SlawekKalandyk <slawomir.kalandyk@gmail.com>
2018-05-03 17:13:38 +0200

    up

--- lab8/moviesubtitles/MovieSubtitles.cpp
+++ lab8/moviesubtitles/MovieSubtitles.cpp
@@ -3,3 +3,33 @@
 //
 
 #include "MovieSubtitles.h"
+
+namespace moviesubs {
+
+    void MicroDvdSubtitles::ShiftAllSubtitlesBy(int offset_in_micro_seconds, int frame_per_second,
+                                                std::istream *in, std::ostream *out) {
+        string input, tmp;
+        int frame_offset = (offset_in_micro_seconds * frame_per_second) / 1000;
+        int i = 0;
+
+        while(getline(*in, input)) {
+            for(int j = 0; j < 2; ++j) {
+                *out << "{";
+
+                while (input[i] != '}') {
+                    tmp += input[i];
+                    ++i;
+                }
+
+                ++i;
+                tmp = tmp.substr(1);
+                *out << to_string(stoi(tmp) + frame_offset) << "}";
+                tmp = "";
+            }
+
+            *out << input.substr(i) <<"\n";
+            input = "";
+            i = 0;
+        }
+    }
+}
--- lab8/moviesubtitles/MovieSubtitles.h
+++ lab8/moviesubtitles/MovieSubtitles.h
@@ -6,14 +6,61 @@
 #define JIMP_EXERCISES_MOVIESUBTITLES_H
 
 #include <stdexcept>
+#include <string>
+#include <sstream>
+#include <iostream>
 
 using ::std::stringstream;
+using ::std::string;
+using ::std::getline;
+using ::std::cout;
+using ::std::endl;
+using ::std::to_string;
+using ::std::stoi;
 
 namespace moviesubs {
-    class MicroDvdSubtitles {
+    class MovieSubtitles {
     public:
-        void ShiftAllSubtitlesBy();
+        virtual void ShiftAllSubtitlesBy(int offset_in_micro_seconds, int frame_per_second,
+                                         std::istream *in, std::ostream *out) =0;
+
+        virtual ~MovieSubtitles() = default;
     };
+
+    class MicroDvdSubtitles : public MovieSubtitles {
+    public:
+        void ShiftAllSubtitlesBy(int offset_in_micro_seconds, int frame_per_second,
+                                 std::istream *in, std::ostream *out) override;
+    };
+
+    class SubtitlesException : public std::invalid_argument {
+    public:
+        SubtitlesException();
+
+        SubtitlesException(int lineNum, string line);
+
+        virtual int LineAt() const;
+
+    };
+
+    class NegativeFrameAfterShift : public SubtitlesException {
+    public:
+    };
+
+    class SubtitleEndBeforeStart : public SubtitlesException {
+    public:
+    };
+
+    class InvalidSubtitleLineFormat : public SubtitlesException {
+    public:
+    };
+
+    class SubRipSubtitles : public MovieSubtitles {
+    public:
+        void ShiftAllSubtitlesBy(int offset_in_micro_seconds, int frame_per_second,
+                                 std::istream *in, std::ostream *out) override;
+    };
+
 }
 
 
