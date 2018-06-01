7ef4529ed61fe31280fcab78873a9417807a4e58
SlawekKalandyk <slawomir.kalandyk@gmail.com>
2018-06-01 19:04:26 +0200

    time recorder done

--- lab12/timerecorder/TimeRecorder.h
+++ lab12/timerecorder/TimeRecorder.h
@@ -14,6 +14,17 @@ using ::std::make_pair;
 
 namespace profiling {
 
+    template<typename T>
+    auto TimeRecorder(T obj) {
+        auto before = std::chrono::high_resolution_clock::now();
+        auto temp = obj();
+        auto after = std::chrono::high_resolution_clock::now();
+
+        auto toMiliSec = std::chrono::duration_cast<std::chrono::duration<double>>
+                                  (after - before).count() * 1000;
+
+        return make_pair(temp, toMiliSec);
+    };
 }
 
 
